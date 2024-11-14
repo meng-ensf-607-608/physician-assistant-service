a28ffb7881c0639d630e6104936fe873c4e68162
harneetchahal <harneetchahal1998@gmail.com>
2024-11-14 12:28:22 -0700

    updated web config

--- src/main/java/com/harneet/ucal/physicianAssistantService/config/WebConfig.java
+++ src/main/java/com/harneet/ucal/physicianAssistantService/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
                         .allowedOrigins("*") // Allowed origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                         .allowedHeaders("*") // Allowed headers
-                        .allowCredentials(true); // Allow credentials
+                        .allowCredentials(false); // Allow credentials
             }
         };
     }
